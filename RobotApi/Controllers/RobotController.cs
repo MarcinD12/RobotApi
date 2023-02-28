bcba98d21313d81d2feed5b436941e4e09e99409
MarcinD12 <dickdaring0@gmail.com>
2023-02-28 21:28:23 +0100

    initial commit with working api

---
+++ RobotApi/Controllers/RobotController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RobotApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RobotController : Controller
+    {
+        private static string Command { get; set; }
+
+        [HttpGet("info")]
+        public string GetInfo()
+        {
+            var com = Command;
+            Command = "stop";
+            return com;
+        }
+        [HttpPut("SetCommand")]
+        public void SetCommand(string command) 
+        {
+            
+            Command = command;
+            Console.WriteLine("api command: "+Command );
+        }
+
+    }
+}
