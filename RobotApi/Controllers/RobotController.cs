c450e4dda0541c0c9c7e2d3c2361e5aab92b4142
MarcinD12 <dickdaring0@gmail.com>
2023-04-05 21:08:17 +0200

    Fixed problem with http redirection, added endpoint for radar

--- RobotApi/Controllers/RobotController.cs
+++ RobotApi/Controllers/RobotController.cs
@@ -8,9 +8,15 @@ namespace RobotApi.Controllers
     {
         //private static string Command { get; set; }
         private static Dictionary<string, string> DataPackage = new Dictionary<string, string>() {{ "command", "stop"},{ "angle", "0" } };
+        private static double[] RadarData=new double[20];
 
+        [HttpGet("GetRadarData")]
+        public string GetRadarData()
+        {
+            return JsonSerializer.Serialize(RadarData);
+        } 
         [HttpGet("getcommands")]
-        public string GetData()
+        public string GetCommands()
         {
             System.Diagnostics.Debug.WriteLine("Get Time:" + DateTime.Now);
             var package = JsonSerializer.Serialize(DataPackage).ToString();
@@ -18,19 +24,26 @@ namespace RobotApi.Controllers
             return package;
         }
         [HttpPut("SetTurn")]
-        public void SetTurn(string direction,int angle) 
+        public void SetTurn([FromBody]Dictionary<string,string> content) 
         {
-            System.Diagnostics.Debug.WriteLine("Put Time:"+DateTime.Now);
-            DataPackage["command"] = direction;
-            DataPackage["angle"] = angle.ToString();
+            
+            DataPackage["command"] = content["direction"];
+            DataPackage["angle"] = content["angle"];
+
             Console.WriteLine("api command: "+ DataPackage["command"] + DataPackage["angle"]);
         }
         [HttpPut("SetDirection")]
-        public void SetDirection(string command)
+        public void SetDirection([FromBody]string command)
         {
-            System.Diagnostics.Debug.WriteLine("Put Time:" + DateTime.Now);
+            Console.WriteLine(command);
             DataPackage["command"] = command;
             Console.WriteLine("api command: " + DataPackage["command"]);
+        }
+        [HttpPost("RadarData")]
+        public void ReceiveRadar([FromBody]double[] data)
+        {
+            RadarData = data;
+            Console.WriteLine(data);
         }
 
     }
